6eb44e5011faf6e1a7facd2443ab33bae44f0339
Yan Siegers <email@yansiegers.nl>
2020-11-26 15:38:08 +0100

    Add Tailwind CSS theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    // future: {
+    //     removeDeprecatedGapUtilities: true,
+    //     purgeLayersByDefault: true,
+    //     defaultLineHeights: true,
+    //     standardFontWeights: true
+    // },
+    theme: {
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+            serif: ['Lora', 'serif'],
+            cursive: ['Coming Soon', 'cursive']
+        },
+        colors: {
+            'tp-plants': '#84A882',
+            'tp-mint': '#84A882',
+            'tp-rose': '#F5DAD5',
+            'tp-ocean': '#87A2A8',
+            'tp-sky': '#D8E8F2',
+
+            // indigo: {
+            //     light: '#b3bcf5',
+            //     DEFAULT: '#5c6ac4',
+            //     dark: '#202e78',
+            // }
+        },
+    },
 }
