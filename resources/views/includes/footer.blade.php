44c1389abbd1b27834ea5eaf30666fd35d715464
Yan Siegers <email@yansiegers.nl>
2020-12-09 18:13:00 +0100

    Add tailwind utilities (headings, links, and buttons) and config

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,18 +1,26 @@
-<footer class="container mx-auto p-5 bg-tp-plants">
+<footer class="container mx-auto p-5 bg-tp-plants font-sans">
     <div class="grid grid-cols-2 mb-10">
         <ul>
-            <li>© {{ Date('Y') }} Toekomstplanner</li>
-            <li>Over Toekomstplanner</li>
+            <li><a class="no-link" href="#">© {{ Date('Y') }} Toekomstplanner</a></li>
+            <li><a class="no-link" href="#">Over Toekomstplanner</a></li>
         </ul>
         <ul class="text-right">
-            <li>Algemene voorwaarden</li>
-            <li>Privacybeleid</li>
+            <li><a class="no-link" href="#">Algemene voorwaarden</a></li>
+            <li><a class="no-link" href="#">Privacybeleid</a></li>
         </ul>
     </div>
     <ul class="grid grid-cols-6">
-        <li class="justify-self-center col-start-2"><img src="/assets/icons/outline/chat-alt.svg" alt=""></li>
-        <li class="justify-self-center"><img src="/assets/icons/outline/device-mobile.svg" alt=""></li>
-        <li class="justify-self-center"><img src="/assets/icons/outline/user-group.svg" alt=""></li>
-        <li class="justify-self-center"><img src="/assets/icons/outline/globe.svg" alt=""></li>
+        <li class="justify-self-center col-start-2">
+            <a href="#"><img class="svg" src="/assets/icons/outline/chat-alt.svg" alt=""></a>
+        </li>
+        <li class="justify-self-center">
+            <a href="#"><img class="svg" src="/assets/icons/outline/device-mobile.svg" alt=""></a>
+        </li>
+        <li class="justify-self-center">
+            <a href="#"><img class="svg" src="/assets/icons/outline/user-group.svg" alt=""></a>
+        </li>
+        <li class="justify-self-center">
+            <a href="#"><img class="svg" src="/assets/icons/outline/globe.svg" alt=""></a>
+        </li>
     </ul>
 </footer>
