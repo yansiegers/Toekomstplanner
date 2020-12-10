09805462a121854478cdf48f3a3a7ce075d65f5a
Yan Siegers <email@yansiegers.nl>
2020-12-10 13:28:10 +0100

    Clean up

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,15 +1,15 @@
 <footer class="container mx-auto p-5 bg-tp-plants font-sans">
-    <div class="grid grid-cols-2 mb-10">
+    <div class="grid grid-cols-2">
         <ul>
-            <li><a class="no-link" href="#">© {{ Date('Y') }} Toekomstplanner</a></li>
-            <li><a class="no-link" href="#">Over Toekomstplanner</a></li>
+            <li class="mb-3"><a class="no-link" href="#">© {{ Date('Y') }} Toekomstplanner</a></li>
+            <li class="mb-3"><a class="no-link" href="#">Over Toekomstplanner</a></li>
         </ul>
         <ul class="text-right">
-            <li><a class="no-link" href="#">Algemene voorwaarden</a></li>
-            <li><a class="no-link" href="#">Privacybeleid</a></li>
+            <li class="mb-3"><a class="no-link" href="#">Algemene voorwaarden</a></li>
+            <li class="mb-3"><a class="no-link" href="#">Privacybeleid</a></li>
         </ul>
     </div>
-    <ul class="grid grid-cols-6">
+    <ul class="grid grid-cols-6 mt-5">
         <li class="justify-self-center col-start-2">
             <a href="#"><img class="svg" src="/assets/icons/outline/chat-alt.svg" alt=""></a>
         </li>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,15 +3,15 @@
 @section('content')
     <div class="container mx-auto p-5 grid grid-cols-6 gap-y-8">
         <section class="col-span-full">
-            <img class="" src="/assets/illustrations/undraw_Questions_re_1fy7.svg" alt="">
-            <h1 class="text-2xl text-center font-bold mt-5">Plan de toekomst van je studie</h1>
+            <img src="/assets/illustrations/undraw_Questions_re_1fy7.svg" alt="">
+            <h1 class="mt-5">Plan de toekomst van je studie</h1>
         </section>
 
         <button class="btn btn-primary col-span-4 col-start-2">Aanmelden</button>
-        <p class="col-span-4 col-start-2">Heb je al een account? <a class="hover:text-tp-plants underline" href="/">Log hier in.</a></p>
+        <p class="col-span-4 col-start-2">Heb je al een account? <a href="/">Log hier in.</a></p>
 
-        <section class="col-span-4 col-start-2 ">
-            <h2>Over de Toekomstplanner</h2>
+        <section class="col-span-4 col-start-2">
+            <h2 class="mb-3">Over de Toekomstplanner</h2>
             <p class="pb-5">Als CMD'er weet ik maar al te goed hoe het is om je eigen studieloopbaan te bepalen. Hoewel dat bij mij van een leien dakje is gegaan, is dat voor veel studenten wel anders. Daar kwam ik achter tijdens de introductie van de minor Concepting (semester 1, 2020/2021), waarbij elke student ging nadenken over hun toekomst.</p>
             <p class="pb-5"><i>Weten wat je wilt is niet gemakkelijk.</i> Daarom kwam ik met het plan om daar een oplossing voor te bedenken. Wat zou er gebeuren als studenten wat hulp zouden krijgen bij het uitvogelen van hun toekomstplan? Waar hebben ze precies hulp bij nodig? En hoe zou ik dat dan kunnen aanpakken?</p>
             <p class="pb-5">Allereerst zou de Toekomstplanner een vermindering van studievertraging teweeg kunnen brengen. Door dat studenten oefeningen doen waarbij ze inzicht krijgen in hun toekomst kunnen zij veel gerichter hun studie doorlopen. Natuurlijk zijn die oefeningen erg persoonlijk, en zouden zij die op een centrale plek moeten kunnen invullen. Wat is er dan beter dan een speciale online tool die hen daarbij helpt? Daar gaat dit project over.</p>
