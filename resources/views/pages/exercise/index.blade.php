66cbb72e0278d07d2372800c57d15c5a9f2b4da1
Yan Siegers <email@yansiegers.nl>
2020-12-15 17:50:55 +0100

    Style exercises page for mobile

--- resources/views/pages/exercise/index.blade.php
+++ resources/views/pages/exercise/index.blade.php
@@ -7,19 +7,29 @@
 ])
 
 @section('content')
-    <div class="container mx-auto p-5 grid grid-cols-6 gap-y-8">
-        <section class="col-span-full">
-            <h1 class="text-xl text-center font-bold mt-5">{{ $page_title }}</h1>
+    <div class="container mx-auto p-5 grid grid-cols-6 gap-x-8 gap-y-16">
+        <h1 class="col-span-full">{{ $page_title }}</h1>
 
-            <ol class="list-disc list-inside">
-                @foreach ($exercises as $exercise)
-                    <li>
-                        <a href="{{ action('App\Http\Controllers\ExerciseController@show', [$exercise->id]) }}">
-                            {{ $exercise->exercise_intro->title }}
-                        </a>
-                    </li>
-                @endforeach
-            </ol>
+        <section class="col-span-full">
+            <img src="/assets/images/forest-1245692_1920.jpg" alt="Ikigai">
+            <h2 class="mt-4">Ikigai</h2>
+            <p>
+                Ikigai is een Japans concept dat staat voor datgene waar jij je bed voor uitkomt en waard is om voor te leven. Het is je passie waarin vier elementen samenkomen: hetgeen waar je van houdt, wat de wereld nodig heeft...
+                <a class="btn btn-primary block mt-4" href="{{ action('App\Http\Controllers\ExerciseController@show', [1]) }}">Meer lezen</a>
+            </p>
         </section>
+
+        {{--@foreach ($exercises as $exercise)--}}
+        <section class="col-span-full">
+            {{--<h2>{{ $exercise->exercise_intro->title }}--}}{{--</h2>--}}
+            <h2>De Noordster</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <a class="btn btn-secondary block mt-4" href="#">Meer lezen</a>
+                {{--<a href="{{ action('App\Http\Controllers\ExerciseController@show', [$exercise->id]) }}">Meer lezen</a>--}}
+            </p>
+        </section>
+        {{--@endforeach--}}
+
     </div>
 @stop
