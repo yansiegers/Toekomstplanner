d6e0b0bd1353ef7a6854a7f41997adb386250b73
Yan Siegers <email@yansiegers.nl>
2020-12-15 17:23:39 +0100

    Update menu blade to comply with design

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,23 +1,30 @@
 <div class="bg-tp-plants font-sans mb-8">
     <nav class="container mx-auto p-5 flex justify-between items-center">
         <a class="no-link text-2xl font-bold" href="/">Toekomstplanner</a>
-        <ol class="lg:flex items-center hidden">
-            <li class="px-5 text-xl">
-                <a class="no-link" href="/">
+        <ol class="lg:flex items-center hidden text-xl">
+            <li class="px-5">
+                <a class="no-link" href="{{ action('App\Http\Controllers\ExerciseController@index') }}">
                     <img class="inline mr-2" src="/assets/icons/outline/light-bulb.svg" alt="">Oefeningen
                 </a>
             </li>
-            <li class="px-5 text-xl">
-                <a class="no-link" href="/">
+            <li class="px-5">
+                <a class="no-link" href="{{ route('home') }}">
                     <img class="inline mr-2" src="/assets/icons/outline/book-open.svg" alt="">Blog
                 </a>
             </li>
         </ol>
-        <a class="btn btn-secondary hidden lg:inline-block" href="{{ route('oefeningen.invullen.index', [1]) }}">
+        <a class="btn btn-secondary hidden lg:inline-block" href="{{ route('dashboard') }}">
             <img class="inline mr-1" src="/assets/icons/outline/key.svg" alt="">Inloggen
         </a>
-        <a class="flex lg:hidden" href="{{ route('menu') }}">
-            <img src="/assets/icons/outline/menu.svg" alt="Menu icon">
-        </a>
+
+        @if(isset($menu))
+            <a class="flex lg:hidden" href="{{ route('home') }}">
+                <img src="/assets/icons/outline/x.svg" alt="Close icon">
+            </a>
+        @else
+            <a class="flex lg:hidden" href="{{ route('menu') }}">
+                <img src="/assets/icons/outline/menu.svg" alt="Menu icon">
+            </a>
+        @endif
     </nav>
 </div>
--- resources/views/pages/menu.blade.php
+++ resources/views/pages/menu.blade.php
@@ -3,23 +3,35 @@
 @endphp
 
 @extends('layouts.general', [
-  'title' => $page_title
+  'title' => $page_title,
+  'menu' => True
 ])
 
 @section('content')
-    <div class="container mx-auto p-5 grid grid-cols-6 gap-y-8">
-        <section class="col-span-full">
-            <h1 class="text-xl text-center font-bold mt-5">{{ $page_title }}</h1>
-        </section>
+    <div class="container mx-auto p-5 grid grid-cols-6 gap-8 text-2xl">
+        <ol class="col-start-2 col-span-4">
+            <li class="py-5">
+                <a class="" href="/">
+                    <img class="inline h-12 mr-8" src="/assets/icons/outline/map.svg" alt="">Jaarplan
+                </a>
+            </li>
+            <li class="py-5">
+                <a class="" href="{{ action('App\Http\Controllers\ExerciseController@index') }}">
+                    <img class="inline h-12 mr-8" src="/assets/icons/outline/light-bulb.svg" alt="">Oefeningen
+                </a>
+            </li>
+            <li class="py-5">
+                <a class="" href="{{ route('home') }}">
+                    <img class="inline h-12 mr-8" src="/assets/icons/outline/book-open.svg" alt="">Blog
+                </a>
+            </li>
 
-        <section class="col-span-4 col-start-2">
-            <ol>
-                <li><a href="{{ route('home') }}">Home</a></li>
-                <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                <li><a href="{{ action('App\Http\Controllers\ExerciseController@show', [1]) }}">Oefening intro</a></li>
-                <li><a href="{{ action('App\Http\Controllers\ExerciseAssignmentController@index', [1]) }}">Oefening invullen</a></li>
-                <li><a href="{{ action('App\Http\Controllers\ExerciseResultController@index', [1]) }}">Oefening resultaten</a></li>
-            </ol>
-        </section>
+            <li class="py-5"><a href="{{ action('App\Http\Controllers\ExerciseController@show', [1]) }}">Oefening intro</a></li>
+            <li class="py-5"><a href="{{ action('App\Http\Controllers\ExerciseAssignmentController@index', [1]) }}">Oefening invullen</a></li>
+            <li class="py-5"><a href="{{ action('App\Http\Controllers\ExerciseResultController@index', [1]) }}">Oefening resultaten</a></li>
+        </ol>
+        <a class="btn btn-secondary block col-span-full mt-8" href="{{ route('dashboard') }}">
+            <img class="inline mr-1" src="/assets/icons/outline/key.svg" alt="">Inloggen
+        </a>
     </div>
 @stop
