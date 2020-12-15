312ce907a3d53825a8ff3bf5d89c92bbd760ea59
Yan Siegers <email@yansiegers.nl>
2020-12-15 16:15:39 +0100

    Redo footer to be neat on desktop

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,28 +1,24 @@
-<div class="bg-tp-plants font-sans mt-24">
-    <footer class="container mx-auto p-5">
-        <div class="grid grid-cols-2">
-            <ul>
-                <li class="mb-3"><a class="no-link" href="#">© {{ Date('Y') }} Toekomstplanner</a></li>
-                <li class="mb-3"><a class="no-link" href="#">Over Toekomstplanner</a></li>
-            </ul>
-            <ul class="text-right">
-                <li class="mb-3"><a class="no-link" href="#">Algemene voorwaarden</a></li>
-                <li class="mb-3"><a class="no-link" href="#">Privacybeleid</a></li>
-            </ul>
-        </div>
-        <ul class="grid grid-cols-6 mt-5">
-            <li class="justify-self-center col-start-2">
-                <a href="#"><img class="svg" src="/assets/icons/outline/chat-alt.svg" alt=""></a>
-            </li>
-            <li class="justify-self-center">
-                <a href="#"><img class="svg" src="/assets/icons/outline/device-mobile.svg" alt=""></a>
-            </li>
-            <li class="justify-self-center">
-                <a href="#"><img class="svg" src="/assets/icons/outline/user-group.svg" alt=""></a>
-            </li>
-            <li class="justify-self-center">
-                <a href="#"><img class="svg" src="/assets/icons/outline/globe.svg" alt=""></a>
-            </li>
-        </ul>
-    </footer>
-</div>
+<footer class="grid grid-cols-6 grid-flow-row-dense bg-tp-plants font-sans mt-24">
+    <ul class="col-span-3 lg:col-span-2 pt-5 pl-5">
+        <li class="lg:inline mb-3 lg:mr-4 font-bold"><a class="no-link" href="#">© {{ Date('Y') }} Toekomstplanner</a></li>
+        <li class="lg:inline mb-3"><a class="no-link" href="#">Over Toekomstplanner</a></li>
+    </ul>
+    <ul class="col-span-3 lg:col-start-5 lg:col-span-2 pt-5 pr-5 text-right">
+        <li class="lg:inline mb-3 lg:mr-4"><a class="no-link" href="#">Algemene voorwaarden</a></li>
+        <li class="lg:inline mb-3"><a class="no-link" href="#">Privacybeleid</a></li>
+    </ul>
+    <ul class="col-span-full lg:col-start-3 lg:col-span-2 grid grid-cols-6 p-5">
+        <li class="justify-self-center col-start-2">
+            <a href="#"><img class="svg" src="/assets/icons/outline/chat-alt.svg" alt=""></a>
+        </li>
+        <li class="justify-self-center">
+            <a href="#"><img class="svg" src="/assets/icons/outline/device-mobile.svg" alt=""></a>
+        </li>
+        <li class="justify-self-center">
+            <a href="#"><img class="svg" src="/assets/icons/outline/user-group.svg" alt=""></a>
+        </li>
+        <li class="justify-self-center">
+            <a href="#"><img class="svg" src="/assets/icons/outline/globe.svg" alt=""></a>
+        </li>
+    </ul>
+</footer>
